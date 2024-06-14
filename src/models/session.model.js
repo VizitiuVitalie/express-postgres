7dda7a055239e1e29ed102977926745e706f1900
Roman Vizitiu <md.vizitiuroman@gmail.com>
2024-06-14 06:22:08 +0300

    initial version

--- src/models/session.model.js
+++
@@ -1,8 +0,0 @@
-export class Session {
-  constructor(sessionId, userId, accessToken, refreshToken) {
-    this.session_id = sessionId;
-    this.user_id = userId;
-    this.access_token = accessToken;
-    this.refresh_token = refreshToken;
-  }
-}
